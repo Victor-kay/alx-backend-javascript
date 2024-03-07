c63f07e235b3962c2d0fb68b82c2c62f6572563b
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-07 14:34:01 +0000

    Importing the ClassRoom class from 0-classroom.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,14 @@
+// 1-make_classrooms.js
+
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+    const rooms = [
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34)
+    ];
+    return rooms;
+}
+
+export default initializeRooms;

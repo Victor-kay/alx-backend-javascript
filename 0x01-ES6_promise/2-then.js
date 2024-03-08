0d3ac390a9ca8fd047e37c3b35fd8504f1be11c3
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-08 14:29:12 +0000

    2nd trial

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,8 +4,8 @@ function handleResponseFromAPI(promise) {
             console.log("Got a response from the API");
             return { status: 200, body: 'success' };
         })
-        .catch((error) => {
-            console.error("Error:", error);
+        .catch(() => {
+            console.error("Error: Empty response from API");
             return new Error();
         })
         .finally(() => {

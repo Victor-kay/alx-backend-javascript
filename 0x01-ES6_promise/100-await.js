b741b8f715dd93ea7ebb9002f8edeae8694037a7
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-08 17:16:00 +0000

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,24 @@
+import uploadPhoto from './100-photo';
+import createUser from './100-user';
+
+async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse
+    };
+  } catch (error) {
+    console.error(error); // Log the error
+    return {
+      photo: null,
+      user: null
+    };
+  }
+}
+
+export default asyncUploadUser;

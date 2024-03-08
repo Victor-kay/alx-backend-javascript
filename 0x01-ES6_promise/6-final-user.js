d495e373c2461a4dd38d12b0e6a8be37b07314ea
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-08 16:39:30 +0000

    Handling multiple promises returning { <pending> }

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
+      { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
+      { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason }
+    ];
+  } catch (error) {
+    throw new Error(error);
+  }
+}

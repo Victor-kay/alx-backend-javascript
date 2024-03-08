9160d2e87fac9e1742e76ebbe457c0d2ee9d1cf8
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-08 14:44:44 +0000

    2-then.js code file

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,11 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
     return promise
-        .then((response) => {
+        .then(() => {
             console.log("Got a response from the API");
             return { status: 200, body: 'success' };
         })
         .catch(() => {
             console.error("Error: Empty response from API");
             return new Error();
-        })
-        .finally(() => {
-            console.log("Got a response from the API");
         });
 }

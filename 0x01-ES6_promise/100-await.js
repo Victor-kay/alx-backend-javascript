bf96c447e9633b3b60726aca4c71b4731dab01ec
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-08 17:27:44 +0000

    Writing an async function named asyncUploadUser

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,23 +1,20 @@
-import uploadPhoto from './100-photo';
-import createUser from './100-user';
+import uploadPhoto from './uploadPhoto.js';
+import createUser from './createUser.js';
 
 async function asyncUploadUser() {
   try {
-    const photoPromise = uploadPhoto();
+    const photoPromise = uploadPhoto("photo.jpg");
     const userPromise = createUser();
 
-    const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
+    const [photoResponse, userResponse] = await Promise.allSettled([photoPromise, userPromise]);
 
-    return {
-      photo: photoResponse,
-      user: userResponse
-    };
+    const photo = photoResponse.status === 'fulfilled' ? photoResponse.value : null;
+    const user = userResponse.status === 'fulfilled' ? userResponse.value : null;
+
+    return { photo, user };
   } catch (error) {
-    console.error(error); // Log the error
-    return {
-      photo: null,
-      user: null
-    };
+    console.error("An error occurred:", error);
+    return { photo: null, user: null };
   }
 }
 
